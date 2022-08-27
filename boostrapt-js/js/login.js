752739da39f2c7631180f6d493e3485d1b3c050d
unknown <garciamd91@gmail.com>
2022-08-27 17:27:54 -0300

    redirreccion a user y admin

--- boostrapt-js/js/login.js
+++ boostrapt-js/js/login.js
@@ -1,5 +1,18 @@
 
 function log () {
-    let user = getElementById('user').value
-    let pass = getElementById('pass').value
+    let user = document.getElementById('user').value
+    let pass = document.getElementById('pass').value
+    let localSTG = JSON.parse(localStorage.getItem('usuario')) || []
+    let userExist = localSTG.filter(local => local.user === user)
+
+    if (userExist.length > 0) {
+        if(userExist[0].pass === pass){
+            setTimeout( ()=> {
+            userExist[0].role === 'admin' 
+            ?
+            location.href= './homeAdmin.html'
+            :
+            location.href='./homeUser.html'
+        },2000)
+    }
 }
