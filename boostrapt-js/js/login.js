8f1e38256953fa95756f97cfb8cdd76d75f3188f
unknown <garciamd91@gmail.com>
2022-08-27 15:40:10 -0300

    subiendo repo

---
+++ boostrapt-js/js/login.js
@@ -0,0 +1,5 @@
+
+function log () {
+    let user = getElementById('user').value
+    let pass = getElementById('pass').value
+}
---
+++ boostrapt-js/js/register.js
@@ -0,0 +1,49 @@
+//metodo alternativo
+
+// let buttonReg = document.getElementById('buttonReg').addEventListener('click', ()=> {
+//     let user = document.getElementById('user').value
+//     let pass = document.getElementById('pass').value
+//     let checkPass = docuemnt.getElementById('checkPass').value
+
+//     console.log(user)
+//     console.log(pass)
+//     console.log(checkPass)
+// })
+
+function checkReg(){
+    let user = document.getElementById('user').value
+    let pass = document.getElementById('pass').value
+    let checkPass = document.getElementById('checkPass').value
+    
+    let localSTG = JSON.parse(localStorage.getItem('usuario'))||[]
+
+    const array = []
+    
+    if (user === '' || pass === '' || checkPass === '') {
+        console.log('Formulario incompleto')
+    }
+    else {
+        for (let i = 0; i < localSTG.length; i++) {
+            const usuarioEnLocalSTG = localSTG[i];
+            array.push(usuarioEnLocalSTG)
+        }
+
+        let userExist = localSTG.filter(local => local.user === user)
+        console.log('userExist',userExist)
+
+        if (userExist.length === 0){
+            if (pass === checkPass){
+            array.push({id: localSTG.length + 1,user,pass,role:'admin'})
+            localStorage.setItem('usuario',JSON.stringify(array))
+            console.log(array)            
+            }
+            else {
+                alert('Las contraseñass no coinciden')
+            }
+        }
+        if (userExist.length !== 0){
+            alert('Nombre de usuario no disponible')
+        }
+        
+    }
+}
